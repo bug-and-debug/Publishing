6101f4d0bc59adaccece28a01215a93c7c465ae1
Hans Chen <hanschen812@gmail.com>
2018-03-24 09:16:40 +0800

    last changes on dev

--- client/src/app/core/app.config.ts
+++ client/src/app/core/app.config.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
-	public static API_ENDPOINT: string =  "http://18.219.41.26:4000/api/v1";
-	//public static API_ENDPOINT: string =  "http://localhost:4000/api/v1";
+	//public static API_ENDPOINT: string =  "http://18.219.41.26:4000/api/v1";
+	public static API_ENDPOINT: string =  "http://localhost:4000/api/v1";
 
 	public static SOCIAL_LOGIN_CONFIG = {
 	    GOOGLE: '210538138836-9ab2ts9ahupu0pupj1j52iug8tu1vhdc.apps.googleusercontent.com',
